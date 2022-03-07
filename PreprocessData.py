abd6958f4fdb87829f31defade61a09edf1690e2
priyatarigopula02 <93142418+priyatarigopula02@users.noreply.github.com>
2022-03-07 16:22:39 +0100

    preprocessing

---
+++ PreprocessData.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+from utils import *
+
+def add_features(path_to_data):
+    data = pd.read_parquet(path_to_data)
+    new_data = pd.DataFrame(data)
+    new_data.insert(0, 'time_difference', timepoint_difference(data))
+    new_data.insert(0, 'time_start', time_since_start(new_data))
+    new_data.insert(0, 'time_op', time_since_operation(new_data))
+    new_data.insert(0, 'track_time', track_time(new_data))
+    X = new_data[['time_start','time_op','track_time','Unit_4_Power',
+        'Unit_4_Reactive Power', 'Turbine_Guide Vane Opening','Turbine_Pressure Drafttube',
+        'Turbine_Pressure Spiral Casing','Turbine_Rotational Speed','mode',]].values
+    Y = new_data[['Bolt_1_Tensile','Bolt_2_Tensile', 'Bolt_3_Tensile',
+                'Bolt_4_Tensile','Bolt_5_Tensile','Bolt_6_Tensile',]].values
+    np.savez('Input_to_NN.npz', X = X, Y = Y)
+
+
+add_features('data/input_dataset-2.parquet')
---
+++ utils.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+
+
+def time_since_start(df):
+    time_start = np.zeros(len(df))
+    for i in range(len(df)):
+        if i == 0:
+            time_start[i] = 1
+
+        else:
+            if (df['mode'][i] == 'start') & (df['mode'][i - 1] == 'operation') & (df['time_difference'][i] < 500):
+                time_start[i] = 0
+
+            elif (df['mode'][i] == 'start') & (df['mode'][i - 1] == 'start') & (df['time_difference'][i] < 500):
+                time_start[i] = time_start[i - 1] + df['time_difference'][i]
+
+            elif (df['time_difference'][i] > 500):
+                time_start[i] = 0
+
+            elif (df['mode'][i] == 'operation'):
+                time_start[i] = time_start[i - 1]
+
+    return time_start
+
+
+def time_since_operation(df):
+    time_op = np.zeros(len(df))
+    for i in range(len(df)):
+        if i == 0:
+            time_op[i] = 1
+
+        else:
+            if (df['mode'][i] == 'operation'):
+                time_op[i] = time_op[i - 1] + df['time_difference'][i]
+
+            elif (df['mode'][i] == 'start'):
+                time_op[i] = 0
+
+    return time_op
+
+def timepoint_difference(df):
+    tps =np.array(df.index[:])
+    tp_diff =np.diff(tps)/np.timedelta64(1, 's')
+    tp_diff = np.insert(tp_diff, 0 , 1)
+    return tp_diff
+
+
+def track_time(df):
+    tps =np.array(df.index[:])
+    tp_diff =(tps-tps[0])/np.timedelta64(1, 's')
+    return tp_diff
